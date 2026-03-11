4b98c2fa693556c54b87f2e6313e2591d8231abb
Lukian <lukian55d@gmail.com>
2026-03-11 23:16:13 +0000

    feat: configure settings for core app, templates, media and auth

--- forge_of_masterpieces/settings.py
+++ forge_of_masterpieces/settings.py
@@ -117,3 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "core.User"
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
