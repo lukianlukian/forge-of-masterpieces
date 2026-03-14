fc6334e746aa44cde5cbce7b7511f1fb33d988e8
Lukian <lukian55d@gmail.com>
2026-03-14 18:49:24 +0000

    register all models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import *
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(FreelancerProfile)
+class FreelancerProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EmployerProfile)
+class EmployerProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(JobOffer)
+class JobOfferAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
